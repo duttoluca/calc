c0d78db70d2a577338b89bcc3463499dc79bad51
Luca Dutto <duttoluca@gmail.com>
2014-05-20 13:58:03 +0200

    Primi test con la view e html

--- accert/views.py
+++ accert/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from datetime import date
+from math import trunc
 
-# Create your views here.
+from dateutil.relativedelta import relativedelta
+
+from django.shortcuts import render, render_to_response
+
+from forms import AccertamentoForm
+from django.core.urlresolvers import reverse
+
+
+def calcola(request):
+    if request.method == 'POST':
+        form = AccertamentoForm(request.POST)
+        if form.is_valid():
+            tassa = form.cleaned_data['tassa']
+            versato = form.cleaned_data['versato']
+            ugup = form.cleaned_data['ugup']
+            data_pagamento = form.cleaned_data['data_pagamento']
+            data_calcolo = form.cleaned_data['data_calcolo']
+            flag_sprint = form.cleaned_data['flag_sprint']
+            #calcolo giorni/mesi/anni relativamente al pagamento
+            giorni_pagamento = (data_pagamento - ugup).days
+            rel_pag = relativedelta(data_pagamento, ugup)
+            mesi_pagamento, anni_pagamento = rel_pag.months, rel_pag.years
+            #calcolo mesi/anni relativamente al calcolo
+            rel_calc = relativedelta(data_calcolo, ugup)
+            mesi_calcolo, anni_calcolo = rel_calc.months, rel_calc.years
+            # return dati_calcolati
+            data = {'tassa': tassa-versato}
+            return render(request, 'calcola.html', {'form': AccertamentoForm(), 'data': data})
+    else:
+        form = AccertamentoForm()
+
+    return render(request, 'calcola.html', {'form': form, })
--- calc/settings.py
+++ calc/settings.py
@@ -115,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -128,7 +129,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'calc',
+    'accert',
 )
 
 # A sample logging configuration. The only tangible logging
