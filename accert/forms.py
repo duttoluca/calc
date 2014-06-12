39edd1548c7e99e7b7c075b402b91be86e155082
Luca Dutto <duttoluca@gmail.com>
2014-06-12 09:40:20 +0200

    Ripulito forms.py

--- accert/forms.py
+++ accert/forms.py
@@ -18,16 +18,3 @@ class AccertamentoForm(forms.Form):
                                    required=False)
     flag_sprint = forms.BooleanField(label='Accertamento emesso prima del 06/07/2011',
                                      required=False)
-
-
-test_data = {'tassa': 100,
-             'ugup': date(2007, 02, 02),
-             'data_pagamento': date(2008, 02, 02),
-             'flag_sprint': True}
-
-test_data2 = {'tassa': 100,
-             'ugup': date(2007, 02, 02),
-             'data_pagamento': date(2008, 02, 02),
-             'flag_sprint': False,
-             'data_calcolo': date(2014, 05, 05),
-             'versato': 99.99}
